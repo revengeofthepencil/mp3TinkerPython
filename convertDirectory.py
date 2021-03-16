82600f4be33b99b599957af7d59286c04927cbde
awalker <revengeofthepencil@gmail.com>
2021-03-16 14:59:11 -0700

    correct file tags

--- convertDirectory.py
+++ convertDirectory.py
@@ -21,39 +21,42 @@ if(len(sys.argv) >= 1):
     gotOne = False
     # This would print all the files and directories
     for file in filesInDir:
-        if gotOne == False:
-            fullFileName = dirName + '/' + file
-            outFileName = '/tmp/tester_' + str(time()).replace('.','_') + '.mp3'
-            print('I want one! outFileName = "%s", orig name = "%s"' % (outFileName, file))
-            audio = TinyTag.get(fullFileName, image=True)
-            if (audio != None and audio.bitrate != None):
+        if file.endswith((".mp3")):
+            print('got an mp3 %s' % file)
+        elif file.endswith((".m4a")):
+            if gotOne == False:
+                fullFileName = dirName + '/' + file
+                outFileName = '/tmp/out_' + str(time()).replace('.','_') + file.replace('.m4a','.mp3')
+                print('I want one!! outFileName = "%s", orig name = "%s"' % (outFileName, file))
+                audio = TinyTag.get(fullFileName, image=True)
+                if (audio != None and audio.bitrate != None):
 
-                image_data = audio.get_image()
+                    tags = {}
+                    tags['artist'] = audio.artist + "_tester"
+                    tags['title'] = audio.title
+                    tags['albumartist'] = audio.albumartist
+                    tags['album'] = audio.album
+                    tags['year'] = audio.year
+                    tags['date'] = audio.year
+                    tags['disc'] = audio.disc
+                    tags['genre'] = audio.genre
+                    tags['disc_total'] = audio.disc_total
 
-                tags = {}
-                tags['artist'] = audio.artist
-                tags['title'] = audio.title
-                tags['albumartist'] = audio.title
-                tags['album'] = audio.title
-                tags['year'] = audio.title
-                tags['disc'] = audio.title
-                tags['genre'] = audio.title
-                tags['disc_total'] = audio.disc_total
 
-                bitrate = ("%dk" % (int(audio.bitrate)))
+                    bitrate = ("%dk" % (int(audio.bitrate)))
 
-                print("Title:" + audio.title) 
-                print("Album: " + audio.album) 
-                print("Artist: " + audio.artist) 
-                
-                print("Genre:" + audio.genre) 
-                print("Year Released: " + audio.year) 
-                print("Bitrate:" + str(bitrate)) 
-                print("AlbumArtist: " + audio.albumartist) 
-                print("Duration: " + str(audio.duration) + " seconds") 
+                    print("Title:" + audio.title) 
+                    print("Album: " + audio.album) 
+                    print("Artist: " + tags['artist'] ) 
+                    if(audio.genre != None):
+                        print("Genre:" + audio.genre) 
+                    if(audio.year != None):
+                        print("Year Released: " + audio.year) 
+                    print("Bitrate:" + str(bitrate)) 
+                    print("Duration: " + str(audio.duration) + " seconds") 
 
-                convertedVersion = AudioSegment.from_file(fullFileName)
-                convertedVersion.export(outFileName, format="mp3", tags=tags, bitrate=bitrate)
+                    convertedVersion = AudioSegment.from_file(fullFileName)
+                    convertedVersion.export(outFileName, format="mp3", tags=tags, bitrate=bitrate)
 
-                gotOne = True
+                    gotOne = True
         print(file)
